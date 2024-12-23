0f4da21ec190a9141f926a84b2a5b9b459d7e091
virat-18-git <ksprddy6@gmail.com>
2024-12-23 13:04:33 +0530

    hoisting and for in and for of

---
+++ index.js
@@ -0,0 +1,23 @@
+debugger
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// let a=50;
+// let b=60;
+// let c=60;
+
+
+// let arr=["shiva","vamshi","racky","suresh","naresh"]
+// for(i in arr)
+// // {
+// //     console.log(`${+i+1} - ${arr[i]}`)
+// // }
+
+let arr1=["shiva","vamshi","racky","suresh","naresh"]
+for(i of arr1)
+{
+    console.log(`${1} - ${i}`)
+}
+
+
